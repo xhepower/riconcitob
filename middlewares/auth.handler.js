665069f432f0e4616a4f536e5b1c0fe373cf04bb
Mona Lisa <johndoe@example.com>
2023-11-17 20:50:27 -0600

    zxcxcxz

--- middlewares/auth.handler.js
+++ middlewares/auth.handler.js
@@ -3,34 +3,34 @@ const boom = require('@hapi/boom');
 const { config } = require('./../config/config');
 
 function checkApiKey(req, res, next) {
-  const apiKey = req.headers['api'];
-  if (apiKey === config.apiKey) {
-    next();
-  } else {
-    next(boom.unauthorized());
-  }
+	const apiKey = req.headers['api'];
+	if (apiKey === config.apiKey) {
+		next();
+	} else {
+		next(boom.unauthorized('Problemas con el api key'));
+	}
 }
 
 function checkAdminRole(req, res, next) {
-  const user = req.user;
-  console.log(user);
-  if (user.role === 'admin' || user.role === 'GOD') {
-    next();
-  } else {
-    next(boom.unauthorized());
-  }
+	const user = req.user;
+	console.log(user);
+	if (user.role === 'admin' || user.role === 'GOD') {
+		next();
+	} else {
+		next(boom.unauthorized('problemas con el role'));
+	}
 }
 
 function checkRoles(...roles) {
-  roles.push('admin');
-  return (req, res, next) => {
-    const user = req.user;
-    if (roles.includes(user.role)) {
-      next();
-    } else {
-      next(boom.unauthorized());
-    }
-  };
+	roles.push('admin');
+	return (req, res, next) => {
+		const user = req.user;
+		if (roles.includes(user.role)) {
+			next();
+		} else {
+			next(boom.unauthorized('checkroles'));
+		}
+	};
 }
 
 module.exports = { checkApiKey, checkAdminRole, checkRoles };
